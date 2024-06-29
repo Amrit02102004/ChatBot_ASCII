f10ddf73e5bc758e20488858d5e304f0c85d8fbc
Amrit Sundarka <114827768+Amrit02102004@users.noreply.github.com>
2024-06-29 19:34:32 +0530

    Update views.py

--- ChatBot/ChatApp/views.py
+++ ChatBot/ChatApp/views.py
@@ -1,10 +1,12 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 import json
+from django.views.decorators.csrf import csrf_exempt
 
 def index(request):
     return render(request, 'index.html')
-
+    
+@csrf_exempt
 def get_next_char(request):
     if request.method == 'POST':
         data = json.loads(request.body.decode('utf-8'))
